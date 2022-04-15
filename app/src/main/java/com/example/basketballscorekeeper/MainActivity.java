a3f9833ea7c2df71773e6b86acfd96d98ef2a73f
Fauxbear95 <danielgfaubert@gmail.com>
2022-04-15 15:03:53 -0400

    I added an activity bar that includes an about toast message as well as a settings page that provides the option to save your values. I also added the methods to save and retrieve the values.

--- app/src/main/java/com/example/basketballscorekeeper/MainActivity.java
+++ app/src/main/java/com/example/basketballscorekeeper/MainActivity.java
@@ -2,7 +2,12 @@ package com.example.basketballscorekeeper;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.RadioGroup;
@@ -11,7 +16,7 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity
-        implements View.OnClickListener{
+        implements View.OnClickListener, RadioGroup.OnCheckedChangeListener {
 
     //declaring variables
     private int team1Score = 0;
@@ -19,14 +24,17 @@ public class MainActivity extends AppCompatActivity
     Button removeButton, addButton, resetButton;
     TextView team1ScoreTV, team2ScoreTV;
     RadioGroup numPtsRG;
-    int numPts;
+    int numPts = 2;
     Switch teamSwitch;
+    SharedPreferences preferences;
 
-
-    @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        PreferenceManager.setDefaultValues(this, R.xml.settings, false);
+        preferences = PreferenceManager.getDefaultSharedPreferences(this);
+        //preferences.edit().clear().commit(); //this is used in case it's saving a setting that's breaking the app
 
         //references to views
         addButton  = findViewById(R.id.addbutton);
@@ -41,30 +49,95 @@ public class MainActivity extends AppCompatActivity
         addButton.setOnClickListener(this);
         removeButton.setOnClickListener(this);
         resetButton.setOnClickListener(this);
+        numPtsRG.setOnCheckedChangeListener(this);
+    }
+
+    /** This is runs when the program is started and it retrieves my values */
+    public void onStart()
+    {
+        super.onStart();
+        team1Score = preferences.getInt("Team1Score", 0);
+        team1ScoreTV.setText(Integer.toString(team1Score));
+        team2Score = preferences.getInt("Team2Score", 0);
+        team2ScoreTV.setText(Integer.toString(team2Score));
+        int numPts = preferences.getInt("NumPts", 2);
+
+        if (numPts == 1)
+        {
+            numPtsRG.check(R.id._1pointOption);
+        }
+        else if (numPts == 2)
+        {
+            numPtsRG.check(R.id._2pointOption);
+        }
+        else if (numPts == 3)
+        {
+            numPtsRG.check(R.id._3pointOption);
+        }
+        else
+        {
+            numPtsRG.check(R.id._2pointOption);
+        }
+        updateScreen();
+    }
+    /** This is runs when the program is stopped and it saves my values */
+    public void onStop(){
+        SharedPreferences.Editor editor = preferences.edit();
+        boolean saveValues = preferences.getBoolean("save_values_preferences", false);
+        if (saveValues)
+        {
+            editor.putInt("Team1Score", team1Score);
+            editor.putInt("Team2Score", team2Score);
+            editor.putInt("NumPts", numPts);
+        }
+        else
+        {
+            editor.clear();
+            editor.putBoolean("save_values_preferences", false);
+        }
+        //editor.commit();
+        editor.apply();
+        super.onStop();
+    }
+
+    /** This creates the menu for the buttons to be added*/
+    public boolean onCreateOptionsMenu(Menu menu){
+        getMenuInflater().inflate(R.menu.about_menu, menu);
+        return true;
+    }
+
+    /** This is my method to read from the activity bar when a button is clicked*/
+    public boolean onOptionsItemSelected(MenuItem item){
+        long id = item.getItemId();
+        if (id == R.id.about_menu)
+        {
+            Toast.makeText(this,
+                    "Developer: Daniel Faubert \nStudent Number: A00124623\n" +
+                            "Updated: April 14th, 2022",
+                    Toast.LENGTH_LONG).show();
+            return true;
+        }
+        else if (id == R.id.settings_menu)
+        {
+            Intent settings = new Intent(getApplicationContext(), SettingsActivity.class);
+            startActivity(settings);
+            return true;
+        }
+        else
+        {
+            return super.onOptionsItemSelected(item);
+        }
     }
 
     //method that activates anytime something is clicked
-    @Override
+    /** This is my method to register clicks for everything but the radio buttons */
     public void onClick(View view) {
         // variables only necessary within this method
-        long checkedId = numPtsRG.getCheckedRadioButtonId();
         boolean switchState = teamSwitch.isChecked();
 
         //checks which option is selected in the radiobutton group
         // must be done before pts are allocated
-        if (checkedId == R.id._1pointOption){
-            numPts = 1;
-        }
-        else if (checkedId == R.id._2pointOption){
-            numPts = 2;
-        }
-        else if(checkedId == R.id._3pointOption){
-            numPts = 3;
-        }
-        else
-        {
-            numPts = 2;
-        }
+
 
         //checks the state of the switch to decide where to allocate the new pts
         //must be done before the pts are allocated
@@ -111,8 +184,27 @@ public class MainActivity extends AppCompatActivity
         // whenever anything changes
         updateScreen();
     }
+    /** This is my method to update the screen when anything is changed */
     private void updateScreen(){
         team1ScoreTV.setText(Integer.toString(team1Score));
         team2ScoreTV.setText(Integer.toString(team2Score));
+    }
+
+    /** This is my method to update the number of pts variable when a radio button is clicked*/
+    public void onCheckedChanged(RadioGroup radioGroup, int id) {
+        if (id == R.id._1pointOption){
+            numPts = 1;
+        }
+        else if (id == R.id._2pointOption){
+            numPts = 2;
+        }
+        else if(id == R.id._3pointOption){
+            numPts = 3;
+        }
+        else
+        {
+            numPts = 2;
+        }
+        updateScreen();
     }
 }
